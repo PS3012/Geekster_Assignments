8e93179a8d5b48da02dac2cf4cf56639ebc5fc07
PS3012 <pyushsahud123@gmail.com>
2024-08-15 16:49:07 +0530

    User Information Card

---
+++ Card-Project/script.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", () => {
+  let user = localStorage.getItem("user");
+  if (!user) {
+    user = takeUserInformation();
+  }
+  renderUserInformation(JSON.parse(user));
+});
+
+const takeUserInformation = () => {
+  const firstName = prompt("Enter your First Name.");
+  if (!firstName) {
+    alert("First Name is required.");
+    return takeUserInformation();
+  }
+  const lastName = prompt("Enter your Last Name.");
+  if (!lastName) {
+    alert("Last Name is required.");
+    return takeUserInformation();
+  }
+  const country = prompt("Enter your country.");
+  if (!country) {
+    alert("Country is required.");
+    return takeUserInformation();
+  }
+  const phoneNumber = prompt("Enter your Phone Number.");
+  if (!phoneNumber) {
+    alert("Phone Number is required.");
+    return takeUserInformation();
+  }
+  const state = prompt("Enter your State.");
+  if (!state) {
+    alert("State is required.");
+    return takeUserInformation();
+  }
+  const city = prompt("Enter your City.");
+  if (!city) {
+    alert("City is required.");
+    return takeUserInformation();
+  }
+  const village = prompt("Enter your Village.");
+  if (!village) {
+    alert("Village is required.");
+    return takeUserInformation();
+  }
+  const user = {
+    firstName: firstName,
+    lastName: lastName,
+    country: country,
+    phoneNumber: phoneNumber,
+    state: state,
+    city: city,
+    village: village,
+  };
+  localStorage.setItem("user", JSON.stringify(user));
+  return user;
+};
+
+const renderUserInformation = (user) => {
+  const userInfoItems = document.querySelectorAll(".userInfo");
+  userInfoItems.forEach((ele) => {
+    ele.innerText = user[ele.dataset.info];
+  });
+};
