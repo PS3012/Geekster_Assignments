86eadb808a3497ce3b0cd7c92162086ded8bcb7a
PS3012 <pyushsahud123@gmail.com>
2024-08-15 21:28:27 +0530

    Autosave Textarea

--- AutoSave-TextArea/script.js
+++ AutoSave-TextArea/script.js
@@ -1,7 +1,6 @@
 const textarea = document.querySelector("textarea");
 const switcher = document.querySelector(".toggle");
 const hider = document.querySelector(".toggle .hider");
-const title = document.querySelector(".title");
 
 switcher.addEventListener("click", () => {
   hider.classList.toggle("dark");
