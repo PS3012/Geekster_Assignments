f243473daf534ebabdc15bb9aea54be23dc7e021
PS3012 <pyushsahud123@gmail.com>
2024-08-15 21:23:42 +0530

    Autosave Textarea

---
+++ AutoSave-TextArea/script.js
@@ -0,0 +1,32 @@
+const textarea = document.querySelector("textarea");
+const switcher = document.querySelector(".toggle");
+const hider = document.querySelector(".toggle .hider");
+const title = document.querySelector(".title");
+
+switcher.addEventListener("click", () => {
+  hider.classList.toggle("dark");
+  document.body.classList.toggle("dark");
+  title.classList.toggle("dark");
+});
+
+const debounce = (func, delay) => {
+  let debounceTimer;
+  return function (...args) {
+    const context = this;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+  };
+};
+
+const saveTextareaInput = () => {
+  localStorage.setItem("autosaveText", textarea.value);
+};
+
+window.addEventListener("load", () => {
+  const text = localStorage.getItem("autosaveText");
+  if (text) {
+    textarea.value = text;
+  }
+  textarea.focus();
+  textarea.addEventListener("input", debounce(saveTextareaInput, 300));
+});
